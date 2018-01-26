b531bcc764be3254e2a796154bef936fb9a3da66
AWS2-05 <mcolominasrojas@iesesteveterradas.cat>
2018-01-26 19:55:49 +0100

    capitulo 2 y 3

--- app1/resources/views/catalog/show.blade.php
+++ app1/resources/views/catalog/show.blade.php
@@ -6,28 +6,31 @@
 
 	    <div class="col-sm-4 text-right">
 
-	        <img src="{{$pelicula['poster']}}"/>
+	        <img src="{{$pelicula->poster}}"/>
 
 	    </div>
 	    <div class="col-sm-8">
 
-	        <h2>{{ $pelicula['title'] }}</h2>
-	        <span>Año: {{ $pelicula['year'] }}</span>
+	        <h2>{{ $pelicula->title }}</h2>
+	        <span>Año: {{ $pelicula->year}}</span>
 	        <br>
-	        <p>Director: {{ $pelicula['director'] }}</p>
-	        <p><b>Resumen:</b> {{ $pelicula['synopsis'] }}</p>
+	        <p>Director: {{ $pelicula->director }}</p>
+	        <p><b>Resumen:</b> {{ $pelicula->synopsis }}</p>
 
-	        @if( $pelicula['rented'])
+	        @if( $pelicula->rented)
 	        	<p><b>Estado:</b> Película actualmente alquilada</p>
 				<button type="button" class="btn btn-danger">Devolver película</button>
 	        @else
 	        	<p><b>Estado:</b> Película disponible</p>
-				<button type="button" class="btn btn-primary">Película disponible</button>
+				<button type="button" class="btn btn-primary">Alquilar película</button>
 	        @endif
 
-	        <button type="button" class="btn btn-warning">Editar película</button>
-	        <button type="button" class="btn btn-default">Volver al listado</button>
-
+	        <a href="{{url('/catalog/edit/' . $pelicula->id )}}">
+	        	<button type="button" class="btn btn-warning">Editar película</button>
+	        </a>
+	        <a href="{{url('/catalog')}}">
+		        <button type="button" class="btn btn-default">Volver al listado</button>
+		    </a>
 
 	    </div>
 	</div>
